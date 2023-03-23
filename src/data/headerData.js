15f44251984f53c9c9549b4726a422a52094344f
Usama Akbar <118650267+Usama-Akbar@users.noreply.github.com>
2023-03-23 13:53:09 +0500

    skills and profile updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,11 +1,11 @@
 import resume from "../assets/pdf/resume.pdf";
+import profile from "../assets/png/profile.jpeg";
 
 export const headerData = {
   name: "Usama Akbar",
   title: "Web App Developer",
   desciption:
     "I'm Ambitious to my work with the appreciated skills and confidence even under pressure circumstances.I'd like to learn and work with new technologies.",
-  image:
-    "https://media.licdn.com/dms/image/D4D03AQEHn3n_FzkxOA/profile-displayphoto-shrink_800_800/0/1666079100118?e=1678924800&v=beta&t=szAJPlgw3ZxwDnUG89-2rCwa60IyR6NP0zBT08kcwdY",
+  image: profile,
   resumePdf: resume,
 };
