e8932b774ce10c8bddb0f02acff45ea0e1418329
Usama Akbar <usamamehar435@gmail.com>
2023-06-10 11:52:05 -0700

    imaged updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,12 +1,11 @@
 import resume from "../assets/pdf/resume.pdf";
-import profile from "../assets/png/profile.jpeg";
+import profile from "../assets/png/profile.jpg";
 
 export const headerData = {
   name: "Usama Akbar",
   title: "Web App Developer",
   desciption:
     "I'm Ambitious to my work with the appreciated skills and confidence even under pressure circumstances.I'd like to learn and work with new technologies.",
-  image:
-    "https://codepipeline-us-east-2-96194296606.s3.us-east-2.amazonaws.com/mr.usama_akbar_295462827_1187401878778519_9180892262778713149_n.jpg",
+  image: "https://i.ibb.co/HBB93bS/profile.jpg",
   resumePdf: resume,
 };
